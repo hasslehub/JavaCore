d1f10e64e87390f589040e280813de4918f50ca9
hasslehub <66742510+hasslehub@users.noreply.github.com>
2023-09-01 12:16:33 +0300

    Add files via upload

---
+++ sem1/src/main/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.geekbrains.lesson1.regular"},{"l":"ru.geekbrains.lesson1.sample"}];updateSearchResults();
---
+++ sem1/src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.geekbrains.lesson1.regular","l":"Decorator"},{"p":"ru.geekbrains.lesson1.sample","l":"Main"},{"p":"ru.geekbrains.lesson1.regular","l":"OtherClass"}];updateSearchResults();
